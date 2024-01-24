0f8fee67a0c95341d18ff3e88ee66bd14d8cb357
Darío Griffo <dario@paytently.com>
2024-01-24 20:36:37 +0000

    initial commit

---
+++ WebApplication1/Singleton.cs
@@ -0,0 +1,35 @@
+﻿public class Singleton
+{
+    private Scoped _scoped;
+    private Transient _transient;
+
+    public Singleton(Scoped scoped, Transient transient)
+    {
+        _scoped = scoped;
+        _transient = transient;
+        Console.WriteLine("Singleton");
+    }
+}
+
+public class Scoped
+{
+    private static int i = 0;
+    private Transient _transient;
+
+    public Scoped(Transient transient)
+    {
+        _transient = transient;
+        Console.WriteLine("Scoped {0}", i);
+    }
+}
+
+public class Transient
+{
+    private static int i = 0;
+
+    public Transient()
+    {
+        Console.WriteLine("Transient {0}", i++);
+
+    }
+}
